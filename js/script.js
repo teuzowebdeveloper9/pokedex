eb8ca9e97e93d8f6f7e27a046fb6fdefd8e85c2e
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-04-19 18:08:28 -0300

    FINISH

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let pokemonName = document.querySelector(".pokemon-name");
 let pokemonNumber = document.querySelector(".pokemon-number");
+let form = document.getElementById("search-input");
 
 
 const fetchPokemon = async (pokemon) => {
@@ -18,3 +19,30 @@ const renderPokemon = async (pokemon) => {
     pokemonNumber.innerHTML = data.id; 
 };
 
+const searchPokemon = (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault(); // Previne o comportamento padrão do Enter
+        renderPokemon(form.value.toLowerCase()); // Busca o Pokémon com o valor do input
+        form.value = ""; // Limpa o campo de input
+    }
+};
+
+// Adiciona o evento "keydown" ao input
+form.addEventListener("keydown", searchPokemon);
+ 
+function prev(){
+    let prev = pokemonNumber.innerHTML;
+    prev = parseInt(prev); // Converte o valor para um número inteiro
+    if (prev > 1) {
+        prev--;
+        renderPokemon(prev);
+    }
+}
+function next(){
+    let next = pokemonNumber.innerHTML;
+    next = parseInt(next);
+    if (next < 898){
+        next++;
+        renderPokemon(next);
+    }
+}
