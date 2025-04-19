427b4fe59675d45baadcb0af3800a48904ffdc82
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-04-19 17:33:53 -0300

    almost finished

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+let pokemonName = document.querySelector(".pokemon-name");
+let pokemonNumber = document.querySelector(".pokemon-number");
+
+
 const fetchPokemon = async (pokemon) => {
 
-    const API_RESPONSE = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}` )
+    const API_RESPONSE = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}` );
 
-    console.log(API_RESPONSE)
+    const data = await API_RESPONSE.json();
+
+    return data;
 
 }
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
 
-fetchPokemon('55');
+    pokemonName.innerHTML = data.name; 
+    pokemonNumber.innerHTML = data.id; 
+};
+
